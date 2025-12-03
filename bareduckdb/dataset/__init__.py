2734d72dd27ab1cb0077c78ed98d051bd93fceca
Paul Timmins <paul@iqmo.com>
2025-12-03 01:01:59 +0000

    fix: return False is unable to enable dataset support

--- bareduckdb/dataset/__init__.py
+++ bareduckdb/dataset/__init__.py
@@ -11,6 +11,10 @@ from bareduckdb.core.connection_base import ConnectionBase
 from bareduckdb.dataset.backend import (
     register_table,
 )
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 __all__ = [
     "register_table",
@@ -18,17 +22,23 @@ __all__ = [
 
 @cache
 def enable_dataset_support(con: ConnectionBase):
-    # This function is meant to ensure dataset fails immediately and cleanly 
-    # if there's an import error
+    try:
+        # This function is meant to ensure dataset fails immediately and cleanly 
+        # if there's an import error
 
-    # Register arrow_scan_cardinality
-    from ..dataset.impl.dataset import register_dataset_functions_pyx
+        # Register arrow_scan_cardinality
+        from ..dataset.impl.dataset import register_dataset_functions_pyx
 
-    register_dataset_functions_pyx(con._impl)
+        register_dataset_functions_pyx(con._impl)
 
-    import pyarrow as pa
+        import pyarrow as pa
 
-    from bareduckdb.dataset.impl.dataset import (
-        delete_factory_pyx,
-        register_table_pyx,
-    )
+        from bareduckdb.dataset.impl.dataset import (
+            delete_factory_pyx,
+            register_table_pyx,
+        )
+
+        return True
+    except Exception as e:
+        logger.warning("Error enabling dataset support: %s", e)
+        return False
